e1c5365c22915ac069834cbe34f026884053e74e
Hend123 <henda4544@gmail.com>
2020-11-21 18:43:21 +0200

    Clinic management system

--- STD_TYPES.h
+++ STD_TYPES.h
@@ -1,14 +1,11 @@
-#ifndef STD_TYPES_H_INCLUDED
-#define STD_TYPES_H_INCLUDED
 typedef unsigned char uint8;
 typedef unsigned short int uint16;
-typedef unsigned long int uint32;
+typedef unsigned long int  uint32;
+
 typedef signed char sint8;
 typedef signed short int sint16;
 typedef signed long int sint32;
-typedef  float float32;
-typedef  double float64;
-typedef  long double float128;
+typedef  float float32; 
+typedef double float64;
+typedef long double float128;
 
-
-#endif // STD_TYPES_H_INCLUDED
